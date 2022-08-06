cfa0c7f29dea930ed3cdfbd9c10ed168a147c2ea
Dogukan Zorlu <dogukanzrlu@gmail.com>
2022-08-06 16:44:58 +0300

    refactor main by new clang parser

--- main.go
+++ main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
 	"struct-bingen/pkg/clang"
+	_ "struct-bingen/pkg/clang"
+	parser "struct-bingen/pkg/clang/parser"
 )
-
-type sourcePathFlags []string
-
-func (i *sourcePathFlags) String() string {
-	return ""
-}
-
-func (i *sourcePathFlags) Set(value string) error {
-	*i = append(*i, value)
-	return nil
-}
-
-var sourcePaths sourcePathFlags
 
 func main() {
 
-	flag.Var(&sourcePaths, "sourcePath", "add your source path for code")
+	config := readYaml()
 
-	flag.Parse()
-
-	parsed, err := clang.Convert(sourcePaths)
+	_, err := clang.Convert(config)
 	if err != nil {
 		return
 	}
+}
 
-	fmt.Println(parsed.Declarations, err)
+func readYaml() parser.PreProcessConfig {
+	f, err := os.ReadFile("./config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var c parser.PreProcessConfig
+	err = yaml.Unmarshal(f, &c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c
 }
